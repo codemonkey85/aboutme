ce83a37e20c6dc3cbe00230e731ef506232dbf1f
Michael Bond <mbond@3storysoftware.com>
2021-07-28 09:03:51 -0400

    Fix ThemeService not injected error

--- Components/ThemeToggleButton.razor.cs
+++ Components/ThemeToggleButton.razor.cs
@@ -3,20 +3,20 @@ using System.Threading.Tasks;
 
 namespace aboutme.Components
 {
-    public partial class ThemeToggleButton : ComponentBase
+    public partial class ThemeToggleButton
     {
-        protected override async Task OnInitializedAsync()
-        {
-            await ThemeService.ReadThemeFromLocalStorage();
-            await ThemeService.ApplyTheme(RefreshService);
-        }
+        //protected override async Task OnInitializedAsync()
+        //{
+        //    await ThemeService.ReadThemeFromLocalStorage();
+        //    await ThemeService.ApplyTheme(RefreshService);
+        //}
 
-        private async Task SetTheme(bool isDark)
-        {
-            await ThemeService.SetTheme(RefreshService, isDark);
-            RefreshService.CallRequestRefresh();
-        }
+        //private async Task SetTheme(bool isDark)
+        //{
+        //    await ThemeService.SetTheme(RefreshService, isDark);
+        //    RefreshService.CallRequestRefresh();
+        //}
 
-        private async Task SwitchTheme() => await ThemeService.SwitchTheme(RefreshService);
+        //private async Task SwitchTheme() => await ThemeService.SwitchTheme(RefreshService);
     }
 }
