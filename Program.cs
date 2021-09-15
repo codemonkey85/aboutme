f7f80b3b2fa2a8f10038ae5e686f26a58f2bda6a
Michael Bond <codemonkey8510@icloud.com>
2021-09-15 15:44:13 -0400

    Upgrade to .NET 6 part 2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,16 @@
+using aboutme;
 using aboutme.Shared;
 using MarkdownSharp;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace aboutme
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped(sp => new Markdown(new MarkdownOptions { }));
-            builder.Services.AddScoped<JsInterop>();
-            builder.Services.AddScoped<IRefreshService, RefreshService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new Markdown(new MarkdownOptions { }));
+builder.Services.AddScoped<JsInterop>();
+builder.Services.AddScoped<IRefreshService, RefreshService>();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
