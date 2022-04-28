9d2c2f678bf60044c892a8bd38abe569fcd4abc9
Michael Bond <codemonkey8510@icloud.com>
2022-04-28 10:20:34 -0400

    Update packages etc.
    Modernized the app to resemble the structure of a new template, stopped ignoring VSCode stuff, used page titles, etc.

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@ using aboutme;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
