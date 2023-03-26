7b9f7ef100baec715880bb3fb9957aa14397ac7d
Michael Bond <michael@bondcodes.com>
2023-03-26 16:51:07 -0400

    Support dark theme

--- aboutme.library/Shared/MainLayout.razor.cs
+++ aboutme.library/Shared/MainLayout.razor.cs
@@ -3,6 +3,23 @@ namespace aboutme.library.Shared;
 public partial class MainLayout
 {
     protected bool _drawerOpen = true;
+    private bool _isDarkMode;
+    private MudThemeProvider? _mudThemeProvider;
 
     protected void DrawerToggle() => _drawerOpen = !_drawerOpen;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender && _mudThemeProvider is not null)
+        {
+            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
+            await _mudThemeProvider.WatchSystemPreference(OnSystemPreferenceChanged);
+            StateHasChanged();
+        }
+    }
+    private async Task OnSystemPreferenceChanged(bool newValue)
+    {
+        _isDarkMode = newValue;
+        StateHasChanged();
+    }
 }
