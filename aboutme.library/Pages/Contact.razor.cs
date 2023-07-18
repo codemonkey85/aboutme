baa1baa962600a6c8e48fc5138b0773fe169d80f
Michael Bond <michael@bondcodes.com>
2023-07-18 15:23:35 -0400

    Add Threads, add icons

--- aboutme.library/Pages/Contact.razor.cs
+++ aboutme.library/Pages/Contact.razor.cs
@@ -9,6 +9,14 @@ public partial class Contact
             Name = "Mastodon",
             Url = "https://mastodon.social/@codemonkey85",
             Title = "codemonkey85@mastodon.social on Mastodon",
+            Icon = Icons.Material.Filled.OpenInBrowser,
+        },
+        new()
+        {
+            Name = "Threads",
+            Url = "https://www.threads.net/@codemonkey85",
+            Title = "Michael Bond (@codemonkey85) on Threads",
+            Icon = Icons.Material.Filled.OpenInBrowser,
         },
     };
 
@@ -17,5 +25,6 @@ public partial class Contact
         public string? Name { get; set; }
         public string? Url { get; set; }
         public string? Title { get; set; }
+        public string Icon { get; set; } = Icons.Material.Filled.OpenInBrowser;
     }
 }
