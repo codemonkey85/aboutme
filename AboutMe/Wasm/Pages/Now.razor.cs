701731c8daa02b69b586d1b78b221502d8f90fad
Michael <michael@bondcodes.com>
2024-02-19 21:14:00 -0500

    Stub out more now page

--- AboutMe/Wasm/Pages/Now.razor.cs
+++ AboutMe/Wasm/Pages/Now.razor.cs
@@ -2,5 +2,13 @@ namespace AboutMe.Wasm.Pages;
 
 public partial class Now
 {
+    private List<NowThing> NowThings { get; set; } =
+    [
+    ];
+    private struct NowThing
+    {
+        public string Title { get; set; }
 
+        public string Description { get; set; }
+    }
 }
