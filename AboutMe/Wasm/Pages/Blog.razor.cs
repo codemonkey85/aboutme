131c809788384d498ad833bfb72051dab17a3ea3
Michael Bond <michael@bondcodes.com>
2025-01-28 14:18:20 -0500

    Move feed objects, use private

--- AboutMe/Wasm/Pages/Blog.razor.cs
+++ AboutMe/Wasm/Pages/Blog.razor.cs
@@ -33,52 +33,52 @@ public partial class Blog(HttpClient httpClient)
             Console.WriteLine($"Error loading feed: {ex.Message}");
         }
     }
-}
 
-[JsonSerializable(typeof(Feed[]))]
+    [JsonSerializable(typeof(Feed[]))]
+    // ReSharper disable once ClassNeverInstantiated.Global
+    private partial class FeedJsonContext : JsonSerializerContext;
+
+    private readonly record struct Feed
+    {
+        [JsonPropertyName("version")]
+        public string Version { get; init; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; init; }
+
+        [JsonPropertyName("icon")]
+        public string Icon { get; init; }
+
+        [JsonPropertyName("home_page_url")]
+        public string HomePageUrl { get; init; }
+
+        [JsonPropertyName("feed_url")]
+        public string FeedUrl { get; init; }
+
+        // ReSharper disable once CollectionNeverUpdated.Global
+        [JsonPropertyName("items")]
+        public List<Item> Items { get; init; }
+    }
+
 // ReSharper disable once ClassNeverInstantiated.Global
-internal partial class FeedJsonContext : JsonSerializerContext;
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+    private readonly record struct Item
+    {
+        // ReSharper disable once UnusedMember.Global
+        [JsonPropertyName("id")]
+        public string Id { get; init; }
 
-internal readonly record struct Feed
-{
-    [JsonPropertyName("version")]
-    public string Version { get; init; }
+        [JsonPropertyName("title")]
+        public string Title { get; init; }
 
-    [JsonPropertyName("title")]
-    public string Title { get; init; }
+        [JsonPropertyName("content_html")]
+        public string ContentHtml { get; init; }
 
-    [JsonPropertyName("icon")]
-    public string Icon { get; init; }
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        [JsonPropertyName("date_published")]
+        public DateTime DatePublished { get; init; }
 
-    [JsonPropertyName("home_page_url")]
-    public string HomePageUrl { get; init; }
-
-    [JsonPropertyName("feed_url")]
-    public string FeedUrl { get; init; }
-
-    // ReSharper disable once CollectionNeverUpdated.Global
-    [JsonPropertyName("items")]
-    public List<Item> Items { get; init; }
-}
-
-// ReSharper disable once ClassNeverInstantiated.Global
-[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
-internal readonly record struct Item
-{
-    // ReSharper disable once UnusedMember.Global
-    [JsonPropertyName("id")]
-    public string Id { get; init; }
-
-    [JsonPropertyName("title")]
-    public string Title { get; init; }
-
-    [JsonPropertyName("content_html")]
-    public string ContentHtml { get; init; }
-
-    // ReSharper disable once UnusedAutoPropertyAccessor.Global
-    [JsonPropertyName("date_published")]
-    public DateTime DatePublished { get; init; }
-
-    [JsonPropertyName("url")]
-    public string Url { get; init; }
+        [JsonPropertyName("url")]
+        public string Url { get; init; }
+    }
 }
