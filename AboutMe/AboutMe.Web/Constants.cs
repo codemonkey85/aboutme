54f73ae3ef75aec658bfd7b6e8fe26ccc4416803
Michael Bond <michael@bondcodes.com>
2025-05-30 17:24:37 -0400

    Delete everything and start over

--- AboutMe/AboutMe.Web/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AboutMe.Web;
-
-public static class Constants
-{
-    public const string SiteName = "Bond Codes";
-
-    public const string MyGitHubBaseUrl = "https://github.com/codemonkey85";
-}
