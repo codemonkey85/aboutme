ed39fcb084523a6a50ef492e5f87c54ead73890f
Michael Bond <michael@bondcodes.com>
2025-05-30 23:38:33 -0400

    Remove weather test page

--- src/AboutMe/AboutMe.ApiService/Program.cs
+++ src/AboutMe/AboutMe.ApiService/Program.cs
@@ -23,22 +23,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.MapNowEndpoints(airtableClientSettings);
 
 app.MapDefaultEndpoints();
